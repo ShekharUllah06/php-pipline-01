350c1cbda68fcf310e90230b5dcb4ca0b56a9676
Shekhar <aa1912275@gmail.com>
2023-10-02 11:29:32 +0600

    index page

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Jenkis Pipeline</title>
+</head>
+<body>
+  <h1>Hello From  Auto Build</h1>
+  <h1>Welcome to CI/CD</h1>
+</body>
+</html>
